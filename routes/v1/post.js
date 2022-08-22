9b3841f52229453510331b94f04d7f0d54dfa1a9
msamgan <mohdsamgankhan@gmail.com>
2022-08-22 10:51:48 +0400

    added tags api

--- routes/v1/post.js
+++ routes/v1/post.js
@@ -5,7 +5,7 @@ const PostController = require("../../controllers/v1/post.controller")
 
 router.get("/", PostController.index)
 router.get("/tag/:tagSlug", PostController.tag)
-
+router.get("/tags", PostController.tags)
 router.get("/post/:postSlug", PostController.post)
 
 module.exports = router
